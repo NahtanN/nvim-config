2e2af87f6d3b0e8c457c8152238d5daed893d447
Nathan Gomes <nathan.neri@al.senai.br>
2024-07-01 17:19:43 -0300

    custom plugins added

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,15 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+map("x", "<leader>p", [["_dP]])
+map({"n", "v"}, "<leader>d", [["_d]])
+
+map({"n", "v"}, "<leader>y", [["+y]])
+map("n", "<leader>Y", [["+Y]])
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -34,32 +34,28 @@ return {
       require "configs.lspconfig"
     end,
   },
-  -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-telescope/telescope.nvim", lazy = false,
+    config = function ()
+      require "configs.telescope"
+    end
+  },
+  {
+    "ThePrimeagen/harpoon", lazy = false,
+    config = function ()
+      require "configs.harpoon"
+    end
+  },
+  {
+    "mbbill/undotree", lazy = false,
+    config = function ()
+      require "configs.undotree"
+    end
+  },
+  {
+    "tpope/vim-fugitive", lazy = false,
+    config = function ()
+      require "configs.fugitive"
+    end
+  }
 }
