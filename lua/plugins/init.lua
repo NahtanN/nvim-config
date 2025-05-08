8e9af785712474767b49a8d22183ca3f52dda7a7
Nathan Neri <nahtann@outlook.com>
2025-05-08 20:30:42 -0300

    feat: nvim-surround

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -101,5 +101,15 @@ return {
       require("dap-go").setup(opts)
     end
   },
-  { "nvim-treesitter/nvim-treesitter-context", lazy = false }
+  { "nvim-treesitter/nvim-treesitter-context", lazy = false },
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  }
 }
